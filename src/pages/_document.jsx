54e4b14b2d8338c37ab6b9c1217fe53bc36b76b4
Gilang Eka Pratama <104094589+gilangekprtm@users.noreply.github.com>
2023-12-16 09:33:05 +0700

    Update _document.jsx

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -8,6 +8,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Montserrat+Alternates:wght@300;400;500;600;700;800&family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&family=Urbanist:wght@300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="../public/favicon.ico" sizes="64*64" />
       </Head>
       <body>
         <Main />
