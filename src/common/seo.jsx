f7c7fab5edebe77befe7cc2ea254b6dde68a1635
Gilang Eka Pratama <104094589+gilangekprtm@users.noreply.github.com>
2023-12-16 09:44:22 +0700

    Update seo.jsx

--- src/common/seo.jsx
+++ src/common/seo.jsx
@@ -13,7 +13,7 @@ const SEO = ({ pageTitle }) => (
                 name="viewport"
                 content="width=device-width, initial-scale=1, shrink-to-fit=no"
             />
-            <link rel="icon" href="../../public/favicon.ico" />
+            <link rel="icon" href="/favicon.ico" />
         </Head>
     </>
 );
